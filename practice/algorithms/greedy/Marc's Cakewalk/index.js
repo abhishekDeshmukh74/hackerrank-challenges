2e18229c3ca5600de87efb5ef591283e62be6d76
Abhishek Deshmukh <Abhishekdeshmukh74@gmail.com>
2020-02-15 00:17:09 +0530

    solved Marc's Cakewalk

---
+++ practice/algorithms/greedy/Marc's Cakewalk/index.js
@@ -0,0 +1,15 @@
+function marcsCakewalk(calories) {
+
+    calories.sort((a, b) => b - a)
+    let minimum = 0;
+
+    for (let i = 0; i < calories.length; i++) {
+        minimum = minimum + Math.pow(2, i) * calories[i]
+    }
+
+    return minimum
+}
+
+console.log(marcsCakewalk([1, 3, 2]));
+console.log(marcsCakewalk([7, 4, 9, 6]));
+console.log(marcsCakewalk([504, 378, 291, 380, 728, 630, 797, 212, 32, 792, 895, 635, 850, 853, 859, 127, 653, 655, 476, 748]));
