9c7f6b38131146994e8aca49b3b606b4f9f02e66
Abhishek Deshmukh <Abhishekdeshmukh74@gmail.com>
2020-09-13 21:12:40 +0530

    designer PDF Viewer solved

---
+++ practice/algorithms/implementation/designer PDF Viewer/index.js
@@ -0,0 +1,8 @@
+function designerPdfViewer(h, word) {
+    const alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    const wordHeights = word.split('').map(e => h[alphabets.indexOf(e)])
+    return Math.max(...wordHeights) * word.length
+}
+
+console.log(designerPdfViewer([1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5], 'abc'));
+console.log(designerPdfViewer([1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7], 'zeba'));
