3918f2979f38dd7fde98ca5be64e53e300561d53
Abhishek Deshmukh <Abhishekdeshmukh74@gmail.com>
2020-09-25 14:01:19 +0530

    solved character-class regex problems

---
+++ practice/regex/character-class/index.js
@@ -0,0 +1,8 @@
+// matching-range-of-characters
+var Regex_Pattern = /^[a-z][1-9][^a-z][^A-Z][A-Z]/;
+
+// matching-specific-characters
+var Regex_Pattern = /^[123][120][xs0][30Aa][xsu][\.\,]$/;
+
+// excluding-specific-characters
+var Regex_Pattern = /^[^0-9][^aeiou][^bcDF][^\s][^AEIOU][^\.\,]$/;
