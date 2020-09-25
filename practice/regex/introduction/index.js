1ad9f46e534749c5db214d39fe7deab314e281ff
Abhishek Deshmukh <Abhishekdeshmukh74@gmail.com>
2020-09-25 13:21:31 +0530

    solved regex intro problems

---
+++ practice/regex/introduction/index.js
@@ -0,0 +1,17 @@
+// matching specific string
+var Regex_Pattern = 'hackerrank';
+
+// matching-digits-non-digit-character
+var Regex_Pattern = /\d\d\D\d\d\D\d\d\d\d/;
+
+// matching-start-end
+var Regex_Pattern = /^\d\w\w\w\w.$/;
+
+// matching-whitespace-non-whitespace-character
+var Regex_Pattern = /\S\S\s\S\S\s\S\S/;
+
+// matching-word-non-word
+var Regex_Pattern = /\w\w\w\W\w\w\w\w\w\w\w\w\w\w\W\w\w\w/;
+
+// matching-anything-but-new-line
+var Regex_Pattern = /^...\....\....\....$/g;
