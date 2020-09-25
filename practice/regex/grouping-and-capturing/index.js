b008c74c62cadb7fc8bc2005b739c43467231a41
Abhishek Deshmukh <Abhishekdeshmukh74@gmail.com>
2020-09-25 19:54:12 +0530

    solved regex grouping-and-capturing section

---
+++ practice/regex/grouping-and-capturing/index.js
@@ -0,0 +1,8 @@
+// matching-word-boundaries
+var Regex_Pattern = /\b[aeiouAEIOU][\a-zA-Z]*\b/;
+
+// alternative-matching
+var Regex_Pattern = /^(Mr\.|Mrs\.|Ms\.|Dr\.|Er\.)[\a-zA-Z]+$/;
+
+// capturing-non-capturing-groups
+var Regex_Pattern = /(ok){3,}/;
