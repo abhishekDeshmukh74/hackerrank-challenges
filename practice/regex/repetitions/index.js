a548b3bf634d465781ec14b854b6ee9dd5a04ff9
Abhishek Deshmukh <Abhishekdeshmukh74@gmail.com>
2020-09-25 16:44:53 +0530

    solved reprtitions section regex problems

---
+++ practice/regex/repetitions/index.js
@@ -0,0 +1,14 @@
+// matching-one-or-more-repetitions
+var Regex_Pattern = /^[\d]+[A-Z]+[a-z]+$/;
+
+// matching-zero-or-more-repetitions
+var Regex_Pattern = /^[0-9]{2,}[a-z]*[A-Z]*$/;
+
+// matching-x-repetitions
+var Regex_Pattern = /^[a-zA-Z02468]{40}[13579\s]{5}$/;
+
+// matching-x-y-repetitions
+var Regex_Pattern = /^[\d]{1,2}[a-zA-Z]{3,}[/.]{0,3}$/;
+
+// matching-ending-items
+var Regex_Pattern = /^[a-zA-Z]*s$/;
